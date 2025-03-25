bd4461842d5a50afee4f07ae13bfd852df8d22c8
digitalprateek <119813760+digitalprateek@users.noreply.github.com>
2025-03-25 14:09:35 +0530

    Delete src directory
    adding new src

--- src/cloudinaryConfig.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-cloudinary.config({
-  cloud_name: process.env.cloudinaryCloudName,
-  api_key: process.env.cloudApiKey,
-  api_secret: process.env.cloudKeySecret,
-});
-
-module.exports = cloudinary;
